ddf18c8f475977db73bdbc7661200afbdd895ed9
Harold Parra <gc.harold.parra@gmail.com>
2023-03-18 00:46:07 +0000

    Completed basic webapp layout with a faint centered square grid and a confirm button in a bottom menu panel

--- static/js/physarum.js
+++ static/js/physarum.js
@@ -1,50 +1,95 @@
-function createGrid(rows, cols) {
-  const container = document.getElementById("grid-container");
-  const cellSize = container.offsetWidth / cols;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const dot = document.createElement("div");
-      dot.className = "dot";
-      dot.style.width = `${cellSize}px`;
-      dot.style.height = `${cellSize}px`;
-      dot.style.top = `${i * cellSize}px`;
-      dot.style.left = `${j * cellSize}px`;
-      container.appendChild(dot);
+document.addEventListener("DOMContentLoaded", () => {
+  function setCanvasSize() {
+    const canvas = document.getElementById("grid-canvas");
+    const gridContainer = document.querySelector(".grid-container");
+    const canvasSize = Math.min(
+      gridContainer.clientWidth,
+      gridContainer.clientHeight
+    );
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+
+    return canvasSize;
+  }
+  canvasSize = setCanvasSize();
+
+  window.addEventListener("resize", () => {
+    canvasSize = setCanvasSize();
+  });
+
+  function drawGrid(canvasSize) {
+    const canvas = document.getElementById("grid-canvas");
+    const canvasContext = canvas.getContext("2d");
+    const gridSize = 10;
+    const cellSize = canvasSize / gridSize;
+
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        canvasContext.strokeStyle = "rgba(200, 200, 200, 0.5)";
+        canvasContext.strokeRect(
+          j * cellSize,
+          i * cellSize,
+          cellSize,
+          cellSize
+        );
+      }
     }
   }
-}
+  drawGrid(canvasSize);
+});
 
-function setupInteraction() {
-  const dots = document.getElementById("dot");
-  let selectedDot = null;
-  let foodDot = null;
+//function createGrid(rows, cols) {
+//const container = document.getElementById("grid-container");
+//const cellSize = container.offsetWidth / cols;
+//const grid = document.createElement("div");
+//grid.className = "grid";
+//grid.style.gridTemplateRows = rows;
+//grid.style.gridTemplateColumns = cols;
 
-  for (const dot of dots) {
-    dot.addEventListener("click", function () {
-      if (selectedDot) {
-        selectedDot.classList.remove("selected");
-      }
-      if (foodDot && foodDot === dot) {
-        foodDot.classList.remove("food");
-        footDot = null;
-      }
-      selectedDot = dot;
-      dot.classList.add("selected");
-    });
+//for (let i = 0; i < rows; i++) {
+//for (let j = 0; j < cols; j++) {
+//const dot = document.createElement("div");
+//dot.className = "dot";
+//dot.style.width = `${cellSize}px`;
+//dot.style.height = `${cellSize}px`;
+//dot.style.top = `${i * cellSize}px`;
+//dot.style.left = `${j * cellSize}px`;
+//container.appendChild(dot);
+//}
+//}
+//}
 
-    dot.addEventListener("contextmenu", function (e) {
-      e.preventDefault();
-      if (foodDot) {
-        foodDot.classList.remove("food");
-      }
-      if (selectedDot && selectedDot == dot) {
-        selectedDot.classList.remove("selected");
-        selectedDot = null;
-      }
-      foodDot = dot;
-      dot.classList.add("food");
-    });
-  }
-}
+//function setupInteraction() {
+//const dots = document.getElementById("dot");
+//let selectedDot = null;
+//let foodDot = null;
 
-function runPhysarumSimulation() {}
+//for (const dot of dots) {
+//dot.addEventListener("click", function () {
+//if (selectedDot) {
+//selectedDot.classList.remove("selected");
+//}
+//if (foodDot && foodDot === dot) {
+//foodDot.classList.remove("food");
+//foodDot = null;
+//}
+//selectedDot = dot;
+//dot.classList.add("selected");
+//});
+
+//dot.addEventListener("contextmenu", function (e) {
+//e.preventDefault();
+//if (foodDot) {
+//foodDot.classList.remove("food");
+//}
+//if (selectedDot && selectedDot == dot) {
+//selectedDot.classList.remove("selected");
+//selectedDot = null;
+//}
+//foodDot = dot;
+//dot.classList.add("food");
+//});
+//}
+//}
+
+//function runPhysarumSimulation() {}
