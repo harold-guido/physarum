1bab0c0853b3710bc543f8b5746a05d2acb83a3b
Harold Parra <gc.harold.parra@gmail.com>
2023-03-18 16:26:50 +0000

    Implemented hover effect in physarum.js

--- static/js/physarum.js
+++ static/js/physarum.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Reusable constants
+  const canvas = document.getElementById("grid-canvas");
+  const canvasContext = canvas.getContext("2d");
+  const gridSize = 25;
+
+  let hoveredCell = { row: null, col: null };
+
+  // Functions
+
+  // Function to set size of canvas
   function setCanvasSize() {
-    const canvas = document.getElementById("grid-canvas");
     const gridContainer = document.querySelector(".grid-container");
     const canvasSize = Math.min(
       gridContainer.clientWidth,
@@ -11,16 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     return canvasSize;
   }
-  canvasSize = setCanvasSize();
+  let canvasSize = setCanvasSize();
 
-  window.addEventListener("resize", () => {
-    canvasSize = setCanvasSize();
-  });
-
+  // Function to draw square grid on canvas
   function drawGrid(canvasSize) {
-    const canvas = document.getElementById("grid-canvas");
-    const canvasContext = canvas.getContext("2d");
-    const gridSize = 10;
     const cellSize = canvasSize / gridSize;
 
     for (let i = 0; i < gridSize; i++) {
@@ -36,60 +39,107 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   drawGrid(canvasSize);
+
+  // Helper function to debounce events
+  function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+  }
+
+  // Helper function to get grid cell coordinates
+  function getGridCellCoordinates(x, y, canvasSize, gridSize) {
+    const cellSize = canvasSize / gridSize;
+
+    const row = Math.floor(y / cellSize);
+    const col = Math.floor(x / cellSize);
+
+    return { row, col };
+  }
+
+  // Helper function to keep track of hovered cell
+  function trackHoveredCell(row, col) {
+    if (hoveredCell.row == null && hoveredCell.col == null) {
+      hoveredCell.row = row;
+      hoveredCell.col = col;
+
+      check = true;
+
+      return { check, row, col };
+    } else {
+      if (hoveredCell.row != row || hoveredCell.col != col) {
+        prevRow = hoveredCell.row;
+        prevCol = hoveredCell.col;
+
+        hoveredCell.row = row;
+        hoveredCell.col = col;
+
+        check = true;
+
+        return { check, prevRow, prevCol };
+      } else {
+        check = false;
+
+        return { check, row, col };
+      }
+    }
+  }
+
+  // Function to update hover effect
+  function updateHoverEffect(x, y, canvasSize, gridSize, canvasContext) {
+    const { row, col } = getGridCellCoordinates(x, y, canvasSize, gridSize);
+    const { check, prevRow, prevCol } = trackHoveredCell(row, col);
+
+    if (check) {
+      // Clear previously drawn cell
+      canvasContext.clearRect(
+        (prevCol * canvasSize) / gridSize,
+        (prevRow * canvasSize) / gridSize,
+        canvasSize / gridSize,
+        canvasSize / gridSize
+      );
+
+      // Redraw grid lines
+      canvasContext.strokeStyle = "rgba(200, 200, 200, 0.5)";
+      canvasContext.strokeRect(
+        (prevCol * canvasSize) / gridSize,
+        (prevRow * canvasSize) / gridSize,
+        canvasSize / gridSize,
+        canvasSize / gridSize
+      );
+
+      // Draw hover effect
+      canvasContext.fillStyle = "rgba(200, 200, 200, 0.5)";
+      canvasContext.fillRect(
+        (col * canvasSize) / gridSize,
+        (row * canvasSize) / gridSize,
+        canvasSize / gridSize,
+        canvasSize / gridSize
+      );
+    }
+  }
+
+  // Further listeners
+
+  //Mouse moves
+  canvas.addEventListener(
+    "mousemove",
+    debounce((event) => {
+      const canvasRect = canvas.getBoundingClientRect();
+      const x = event.clientX - canvasRect.left;
+      const y = event.clientY - canvasRect.top;
+
+      updateHoverEffect(x, y, canvasSize, gridSize, canvasContext);
+    }, 10)
+  );
+
+  //Click
+
+  //Window resize listener
+  window.addEventListener("resize", () => {
+    canvasSize = setCanvasSize();
+    drawGrid(canvasSize);
+  });
 });
-
-//function createGrid(rows, cols) {
-//const container = document.getElementById("grid-container");
-//const cellSize = container.offsetWidth / cols;
-//const grid = document.createElement("div");
-//grid.className = "grid";
-//grid.style.gridTemplateRows = rows;
-//grid.style.gridTemplateColumns = cols;
-
-//for (let i = 0; i < rows; i++) {
-//for (let j = 0; j < cols; j++) {
-//const dot = document.createElement("div");
-//dot.className = "dot";
-//dot.style.width = `${cellSize}px`;
-//dot.style.height = `${cellSize}px`;
-//dot.style.top = `${i * cellSize}px`;
-//dot.style.left = `${j * cellSize}px`;
-//container.appendChild(dot);
-//}
-//}
-//}
-
-//function setupInteraction() {
-//const dots = document.getElementById("dot");
-//let selectedDot = null;
-//let foodDot = null;
-
-//for (const dot of dots) {
-//dot.addEventListener("click", function () {
-//if (selectedDot) {
-//selectedDot.classList.remove("selected");
-//}
-//if (foodDot && foodDot === dot) {
-//foodDot.classList.remove("food");
-//foodDot = null;
-//}
-//selectedDot = dot;
-//dot.classList.add("selected");
-//});
-
-//dot.addEventListener("contextmenu", function (e) {
-//e.preventDefault();
-//if (foodDot) {
-//foodDot.classList.remove("food");
-//}
-//if (selectedDot && selectedDot == dot) {
-//selectedDot.classList.remove("selected");
-//selectedDot = null;
-//}
-//foodDot = dot;
-//dot.classList.add("food");
-//});
-//}
-//}
-
-//function runPhysarumSimulation() {}
